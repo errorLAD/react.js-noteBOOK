b20e5c374151b46581e60ebbbdbda144f309a242
Abhishek Mishra <ab.mishra@yahoo.com>
2020-01-29 10:11:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NoteTaking from "./components/Note"
+class App extends  Component{ 
+render(){ 
+  return (
+        <div className="">
+          <NoteTaking />
+
+        </div>
+
+  );
+}
+}
+export default App;
